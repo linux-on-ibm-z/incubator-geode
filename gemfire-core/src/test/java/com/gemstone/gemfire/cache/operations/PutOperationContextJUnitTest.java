71b8b7e11e2958e46b3bc49d4efcad69123fd953
Darrel Schneider <dschneider@pivotal.io>
2015-07-10 09:20:47 -0700

    start of unit test

---
+++ gemfire-core/src/test/java/com/gemstone/gemfire/cache/operations/PutOperationContextJUnitTest.java
@@ -0,0 +1,49 @@
+package com.gemstone.gemfire.cache.operations;
+
+import static org.junit.Assert.*;
+import static org.junit.matchers.JUnitMatchers.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.matchers.JUnitMatchers;
+import org.junit.rules.ExpectedException;
+
+import com.gemstone.gemfire.test.junit.categories.UnitTest;
+
+@Category(UnitTest.class)
+public class PutOperationContextJUnitTest {
+
+  @Rule
+  public ExpectedException expectedException = ExpectedException.none();
+  
+  @BeforeClass
+  public static void setUpBeforeClass() throws Exception {
+  }
+
+  @AfterClass
+  public static void tearDownAfterClass() throws Exception {
+  }
+
+  @Before
+  public void setUp() throws Exception {
+  }
+
+  @After
+  public void tearDown() throws Exception {
+  }
+
+  @Test
+  public void testGetSerializedValue() {
+  }
+
+  @Test
+  public void testGetValue() {
+  }
+
+}
